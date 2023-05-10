4e226c6258b123399cc58672547a9d3fc4725834
lawblaze <lawblaze4@gmail.com>
2023-05-10 14:25:39 +0100

    added logic to check for validity of phone number and name

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -23,32 +23,47 @@ export const AddNewMail = async (req, res) => {
         status: false
       });
     }
-
-    // Create a new user
-    const privateCode = v4();
-    const createUser = new User({
-      name,
-      email,
-      phone,
-      sex,
-      code: privateCode
-    });
-
-    // Save the user
-    const userResponse = await createUser.save();
-
-    // Get the count of all users
-    const count = await User.countDocuments({});
-
-    res.json({
-      message: "User created",
-      userNumber: privateCode,
-      status: true,
-      data: userResponse
-    });
-
-    // Send user mail
-    sendUserMail(email, name, privateCode);
+    //check if it is a vaid number
+    if(phone.split('').length < 10){
+      return res.json({
+        message: "Enter a valid phone number",
+        status: false
+      });
+      //check if it is a valid name
+    }else if(name.split('').length < 4){
+      return res.json({
+        message: "Enter a valid name",
+        status: false
+      });
+    }else{
+      // Create a new user
+      const privateCode = v4();
+      const createUser = new User({
+        name,
+        email,
+        phone,
+        sex,
+        code: privateCode
+      });
+  
+      // Save the user
+      const userResponse = await createUser.save();
+  
+      // Get the count of all users
+      const count = await User.countDocuments({});
+  
+      res.json({
+        message: "User created",
+        userNumber: privateCode,
+        status: true,
+        data: userResponse
+      });
+  
+      // Send user mail
+      sendUserMail(email, name, privateCode);
+    }
+    
+   
   } catch (err) {
     res.json({
       message: "An error occurred",
